cbd2c20a96e1d6b73cf9f02ba42cbd14ed6a7e12
Jackson Cuevas <jackson.cuevas@hotmail.com>
2020-05-20 21:54:21 -0400

    Refactored the code

--- MorseCode/MorseTranslator.cs
+++ MorseCode/MorseTranslator.cs
@@ -1,69 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace MorseCode
 {
     public class MorseTranslator
     {
 
+        IDictionary<char, string> dictionary = new Dictionary<char, string>()
+        {
+            {'S', "..."},
+            {'O', "---"},
+            {'E', "."},
+            {'A', ".-"},
+            {'D', "-.."},
+            {'R', ".-."},
+            {'L', ".-.."},
+            {'I', ".."},
+            {'Y', "-.--"},
+            {'W', ".--"},
+            {'N', "-."},
+            {'M', "--"},
+            {'J', ".---"},
+            {'C', "-.-."},
+            {'K', "-.-"},
+            {'B', "-..."},
+            {'T', "-"},
+            {'U', "..-"},
+            {'V', "...-"},
+            {'P', ".--."},
+        };
+
+        private string translatedMessage = String.Empty;
+
         public string Translate(string message)
         {
-
-            string translatedMessage = String.Empty;
-
-            IDictionary<char, string> dictionary = new Dictionary<char, string>() {
-                {'A', ".-"},
-                {'J', ".---"},
-                {'E', "."},
-                {'S', "..." },
-                {'O', "---" },
-                {'D', "-.." },
-                {'R', ".-." },
-                {'I', ".." },
-                {'W', ".--" },
-                {'N', "-." },
-                {'K', "-.-" },
-                {'B', "-..." },
-                {'C', "-.-." },
-                {'V', "...-" },
-                {'P', ".--." },
-                {'M', "--" },
-                {'Y', "-.--" },
-                {'L',".-.."},
-                {'T',"-"},
-                {'U',"..-"}
-            };
-
             if (message == "")
             {
                 return translatedMessage;
             }
 
-            foreach (char letra in message)
+            foreach (char letter in message)
             {
-                if (letra == ' ')
+                if (letter == ' ')
                 {
                     translatedMessage += ' ';
-                }
-                else if (dictionary.ContainsKey(letra))
+                } 
+                else if (dictionary.ContainsKey(letter))
                 {
-                    translatedMessage += dictionary[letra] + "|";
+                    translatedMessage += dictionary[letter] + "|";
                 }
             }
 
             string[] dividedMessage = translatedMessage.Split(' ');
-
+            
             string newMessage = "";
+            
             for (int i = 0; i < dividedMessage.Length; i++)
             {
                 newMessage += dividedMessage[i].Remove(dividedMessage[i].Length - 1) + " ";
             }
 
+
             return newMessage.Remove(newMessage.Length - 1);
         }
     }
-
- 
 }
--- MorseCodeTest/MorseTranslatorTest.cs
+++ MorseCodeTest/MorseTranslatorTest.cs
@@ -1,42 +1,43 @@
 using MorseCode;
 using NUnit.Framework;
-using System;
 
 namespace MorseCodeTest
 {
-    /*
-   * PO Test Cases:
-   * [TestCase("", "")] DONE
-   * [TestCase("E", ".")] DONE
-      [TestCase("SOS", "...|---|...")]
-      [TestCase("SOS SOS", "...|---|... ...|---|...")]
-      [TestCase("ADERLIS Y EDWIN", ".-|-..|.|.-.|.-..|.|... -.-- .|-..|.--|.|-.")]
-      [TestCase("WILSON Y OMAR", ".--|..|.-..|...|---|-. -.-- ---|--|.-|.-.")]
-      [TestCase("JACKSON Y ROBERT", ".---|.-|-.-.|-.-|...|---|-. -.-- .-.|---|-...|.|.-.|-")]
-      [TestCase("SCRUM DEVELOPER", "...|-.-.|.-.|..-|-- -..|.|...-|.|.-..|---|.--.|.|.-.")]
-      [TestCase("ALOES PMS", ".-|.-..|---|.|... .--.|--|...")]
-   */
     [TestFixture]
     public class MorseTranslatorTest
     {
+        /*
+          * PO Test Cases:
+          * [TestCase("", "")] DONE
+          * [TestCase("E", ".")] DONE
+             [TestCase("SOS", "...|---|...")] DONE
+             [TestCase("SOS SOS", "...|---|... ...|---|...")] DONE
+             [TestCase("ADERLIS Y EDWIN", ".-|-..|.|.-.|.-..|.|... -.-- .|-..|.--|.|-.")] DONE
+             [TestCase("WILSON Y OMAR", ".--|..|.-..|...|---|-. -.-- ---|--|.-|.-.")] DONE
+             [TestCase("JACKSON Y ROBERT", ".---|.-|-.-.|-.-|...|---|-. -.-- .-.|---|-...|.|.-.|-")] DONE
+             [TestCase("SCRUM DEVELOPER", "...|-.-.|.-.|..-|-- -..|.|...-|.|.-..|---|.--.|.|.-.")] DONE
+             [TestCase("ALOES PMS", ".-|.-..|---|.|... .--.|--|...")] DONE
+          */
 
-        [TestCase("", "")]
-        public void With_Empty_String_Returns_Empty_String(string message, string tranlation)
+        [TestCase("","")]
+        public void With_Empty_String_Returns_Empty_String(string message, string translation)
         {
             var translator = new MorseTranslator();
 
             string response = translator.Translate(message);
 
-            Assert.That(response, Is.EqualTo(tranlation));
+            Assert.That(response, Is.EqualTo(translation));
         }
 
-        [TestCase("E", ".")]
-        public void With_LetterE_Returns_Dot(string message, string tranlation)
+        [TestCase("E",".")]
+        public void With_Letter_E_Returns_Dot(string message, string translation)
         {
             var translator = new MorseTranslator();
 
             string response = translator.Translate(message);
-            Assert.That(response, Is.EqualTo(tranlation));
+
+            Assert.That(response, Is.EqualTo(translation));
+
         }
 
         [TestCase("SOS", "...|---|...")]
@@ -46,12 +47,13 @@ namespace MorseCodeTest
         [TestCase("JACKSON Y ROBERT", ".---|.-|-.-.|-.-|...|---|-. -.-- .-.|---|-...|.|.-.|-")]
         [TestCase("SCRUM DEVELOPER", "...|-.-.|.-.|..-|-- -..|.|...-|.|.-..|---|.--.|.|.-.")]
         [TestCase("ALOES PMS", ".-|.-..|---|.|... .--.|--|...")]
-        public void With_Values_Returns_Translation(string message, string tranlation)
+        public void With_Values_Returns_Translation(string message, string translation)
         {
             var translator = new MorseTranslator();
 
             string response = translator.Translate(message);
-            Assert.That(response, Is.EqualTo(tranlation));
+
+            Assert.That(response, Is.EqualTo(translation));
         }
 
     }
